7018a8ef6534a7dfd702a0b02b3602cb19f37589
Juan Maria Elsener <juan.elsener1994@gmail.com>
2026-05-18 19:32:07 -0300

    Controlador de Inscripciones Completo y funcionando

---
+++ Controllers/InscripcionesController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Sammasati.App.Data;
+using Sammasati.App.Models;
+
+namespace Sammasati.App.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InscripcionesController : ControllerBase
+    {
+        private readonly SammasatiDbContext _context;
+
+        public InscripcionesController(SammasatiDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Inscripciones
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Inscripcione>>> GetInscripciones()
+        {
+            return await _context.Inscripciones.ToListAsync();
+        }
+
+        // GET: api/Inscripciones/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Inscripcione>> GetInscripcione(int id)
+        {
+            var inscripcione = await _context.Inscripciones.FindAsync(id);
+
+            if (inscripcione == null)
+            {
+                return NotFound();
+            }
+
+            return inscripcione;
+        }
+
+        // PUT: api/Inscripciones/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutInscripcione(int id, Inscripcione inscripcione)
+        {
+            if (id != inscripcione.IdInscripcion)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(inscripcione).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!InscripcioneExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Inscripciones
+        [HttpPost]
+        public async Task<ActionResult<Inscripcione>> PostInscripcione(Inscripcione inscripcione)
+        {
+            _context.Inscripciones.Add(inscripcione);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetInscripcione), new { id = inscripcione.IdInscripcion }, inscripcione);
+        }
+
+        // DELETE: api/Inscripciones/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteInscripcione(int id)
+        {
+            var inscripcione = await _context.Inscripciones.FindAsync(id);
+            if (inscripcione == null)
+            {
+                return NotFound();
+            }
+
+            _context.Inscripciones.Remove(inscripcione);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool InscripcioneExists(int id)
+        {
+            return _context.Inscripciones.Any(e => e.IdInscripcion == id);
+        }
+    }
+}

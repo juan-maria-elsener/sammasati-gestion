e62cc1150ea97b78234f0fb50ef14bb0537882e8
Juan Maria Elsener <juan.elsener1994@gmail.com>
2026-05-21 21:08:22 -0300

    fix: mapeo de columnas y claves foraneas en el modelo Asistencia

--- Models/Asistencia.cs
+++ Models/Asistencia.cs
@@ -1,18 +1,34 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
 
 namespace Sammasati.App.Models;
 
+[Table("asistencias")]
 public partial class Asistencia
 {
+    [Key]
+    [Column("id_asistencia")]
     public int IdAsistencia { get; set; }
+
+    [Column("id_alumno")]
     public int IdAlumno { get; set; }
+
+    [Column("id_clase")]
     public int IdClase { get; set; }
+
+    [Column("fecha")]
     public DateOnly Fecha { get; set; }
+
+    [Column("estado")]
     public string Estado { get; set; } = null!;
 
     [JsonIgnore]
+    [ForeignKey("IdAlumno")]
     public virtual Alumno? IdAlumnoNavigation { get; set; }
+
     [JsonIgnore]
+    [ForeignKey("IdClase")]
     public virtual Clase? IdClaseNavigation { get; set; }
 }
